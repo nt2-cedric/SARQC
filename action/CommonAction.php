156b307f6007da0147f32b03bcff3764a81f068a
Ludovic Doutre-Guay <ludovic@labo-nt2.org>
2020-02-13 10:04:12 -0500

    test

--- action/CommonAction.php
+++ action/CommonAction.php
@@ -14,6 +14,7 @@ abstract class CommonAction
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);
+        echo('test');
         $this->executeAction();
     }
 
